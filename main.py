54597a3e0dbb5da3000ad2f4c5c43a8b01259862
lahiruomadhawa <lahirumadhawa00@gmail.com>
2025-05-26 20:16:27 +1000

    Test with known error again

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 # This is a sample .py file for testing pipeline
 def print_hello():
     print("="*21)
-    print("= Hello from python ="
+    print("= Hello from python ="))
     print("="*21)
 
 
